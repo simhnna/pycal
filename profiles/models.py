b7cd5d1dc82cd9f70ce4374a4968999fe29f3e14
Simon Hanna <simon.h4nn4@gmail.com>
2015-07-22 10:37:20 +0200

    add missing import

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.template import Context
+from django.core.urlresolvers import reverse
 
 import string
 import random
