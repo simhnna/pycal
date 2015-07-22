b10e4d8a35d7d515a5985cbb6b11bcb4cb2f5c44
Simon Hanna <simon.h4nn4@gmail.com>
2015-07-22 10:35:34 +0200

    fix import error v2

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-import django.template.Context
+from django.template import Context
 
 import string
 import random
