88dadd1cd884f23eab9f941281a5a432fb254108
Simon Hanna <simon.h4nn4@gmail.com>
2016-05-16 16:33:16 +0200

    Fix time of recurring events

--- events/funcs.py
+++ events/funcs.py
@@ -40,30 +40,38 @@ class VEvent():
             event.dtstart = source.get('recurrence-id').dt
         if source.get('duration'):
             event.dtend = event.dtstart + source.get('duration').td
-
+        tz = pytz.timezone('Europe/Berlin')
+        if hasattr(event.dtstart, 'tzinfo'):
+            tz = event.dtstart.tzinfo
+            event.dtstart = event.dtstart.replace(tzinfo=None)
         rule = dateutil.rrule.rruleset()
         if source.get('rrule'):
             rrule = source.get('rrule').to_ical().decode('utf-8')
-            rule.rrule(dateutil.rrule.rrulestr(rrule, dtstart=event.dtstart))
+            rule.rrule(dateutil.rrule.rrulestr(rrule,
+                dtstart=event.dtstart, ignoretz=True))
         if source.get('rdate'):
             for date in source.get('rdate').dts:
                 dt = date.dt
-                if hasattr(dt, 'time'):
-                    dt = dt.replace(tzinfo=event.dtstart.tzinfo)
+                if hasattr(dt, 'tzinfo'):
+                    dt = dt.replace(tzinfo=None)
                 rule.rdate(dt)
         if source.get('exrule'):
             exrule = source.get('exrule').to_ical().decode('utf-8')
-            rule.exrule(dateutil.rrule.rrulestr(exrule, dtstart=event.dtstart))
+            rule.exrule(dateutil.rrule.rrulestr(exrule,
+                dtstart=event.dtstart, ignoretz=True))
         if source.get('exdate'):
             for date in source.get('exdate').dts:
                 dt = date.dt
-                if hasattr(dt, 'time'):
-                    dt = dt.replace(tzinfo=event.dtstart.tzinfo)
+                if hasattr(dt, 'tzinfo'):
+                    dt = dt.replace(tzinfo=None)
                 rule.exdate(dt)
         event.last_modified = source.get('last-modified').dt
 
         event.recurrences = set(list(rule))
         event.recurrences.discard(event.dtstart)
+        event.recurrences = [tz.localize(r) for r in event.recurrences]
+        if hasattr(event.dtstart, 'tzinfo'):
+            event.dtstart = tz.localize(event.dtstart)
         return event
 
 
